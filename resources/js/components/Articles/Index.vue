9511fe0e8355d1a5a206f0c0b925338b84350f0b
Ryo Tamashiro <pousse.cafe@gmail.com>
2019-05-06 16:25:05 +0900

    初期化

---
+++ resources/js/components/Articles/Index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        xx
+    </div>
+</template>
+
---
+++ resources/js/components/Layouts/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <li>
+            <router-link  to="/about">About</router-link>
+        </li>
+    </nav>
+</template>
